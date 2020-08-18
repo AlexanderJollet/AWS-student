6c7415423579762cd353d131e5a196fb2b766e95
Jean-Pierre <jp@xiasma.fr>
2020-08-18 14:53:58 +0200

    add pdf3

--- CLI/testb3.py
+++ CLI/testb3.py
@@ -6,6 +6,6 @@ ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
-        #pprint(instance)
+        pprint(instance)
         print(instance["InstanceId"])
         print(instance["PublicIpAddress"])
