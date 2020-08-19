e01d163cf18dbb8a0f2fb7df27dd2a8db27ccf55
Jean-Pierre <jp@xiasma.fr>
2020-08-19 17:41:05 +0200

    update keys

--- TFex_bastion/Scripts/insert_private_key.sh
+++ TFex_bastion/Scripts/insert_private_key.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-data=$( base64 -w 0 ../ssh-keys/id_rsa_bastion )
+data=$( base64 -w 0 ../ssh-keys/id_rsa_instances )
 sed -e "s/^PRIVKEY.*/${data}/" < bastion_init.tmpl.sh > bastion_init.sh
